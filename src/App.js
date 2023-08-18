4ec0bc0951a71e91713916b828b85a9c01c8998a
DmitriyRiabko <riabko97@gmail.com>
2023-08-18 13:45:14 +0300

    create React template

---
+++ src/App.js
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div className="wrapper">
+      <header>
+        <svg></svg>
+        <div>
+          <h3>React Sneakers</h3>
+          <p>Best sneakers marketplace</p>
+        </div>
+      </header>
+  
+    </div>
+  );
+}
+
+export default App;
