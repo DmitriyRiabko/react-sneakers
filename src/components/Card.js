2415aaa341ce23ae19edaacdbe1f914f85a72b7e
DmitriyRiabko <riabko97@gmail.com>
2023-08-20 23:59:12 +0300

    learned: How props works

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,4 @@
-function Card() {
+function Card(props) {
   return (
     <div className="card">
       <div className="favorite">
@@ -6,16 +6,16 @@ function Card() {
       </div>
       <img
         className="d-flex justify-center"
-        src="/img/sneakers/1.jpg"
+        src={props.imgUrl}
         alt="sneakers_1"
         width={133}
         height={112}
       />
-      <h5>Men's sneakers Nike Blazer Mid Suede</h5>
+      <h5>{props.title}</h5>
       <div className="d-flex justify-between align-center">
         <div className="d-flex flex-column">
           <span>Price:</span>
-          <b>$ 139.00</b>
+          <b>$ {props.price}</b>
         </div>
         <button className="button">
           <img width={11} height={11} src="/img/plus.svg" alt="add" />
